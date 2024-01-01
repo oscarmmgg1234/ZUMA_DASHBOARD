64d8d1f4633ac250702f284f922705b9c94f881a
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2023-12-31 19:38:49 -0800

    fix endpoints for employee and layout for inventory

--- src/components/dashboard/pages/inventory/Modals/ViewInventory.js
+++ src/components/dashboard/pages/inventory/Modals/ViewInventory.js
@@ -88,23 +88,29 @@ export default function ViewInventoryModal(props) {
     >
       <td
         className={`${
-          product.focus ? "bg-orange-200 px-4 py-2 border" : "px-4 py-2 border"
+          product.focus
+            ? "bg-orange-300 px-4 py-2 border text-black"
+            : "px-4 py-2 border text-black"
         }`}
       >
         {product.NAME}
       </td>
       <td
         className={`${
-          product.focus ? "bg-orange-200 px-4 py-2 border" : "px-4 py-2 border"
+          product.focus
+            ? "bg-orange-300 px-4 py-2 border text-black"
+            : "px-4 py-2 border text-black"
         }`}
-      >
-        <span className="bg-green-200 rounded-md px-4 py-2">
+      > 
+        <span className="bg-green-300 rounded-md px-4 py-2 text-black">
           {product.PRODUCT_ID}
         </span>
       </td>
       <td
         className={`${
-          product.focus ? "bg-orange-200 px-2 py-1 border" : "px-4 py-2 border"
+          product.focus
+            ? "bg-orange-300 px-2 py-1 border text-black"
+            : "px-4 py-2 border text-black"
         }`}
       >
         {product.COMPANY}
@@ -120,89 +126,106 @@ export default function ViewInventoryModal(props) {
         title={"View Inventory"}
       >
         <div className="container mx-auto p-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="col-span-1 bg-gray-200 p-4">
+          <div className="grid grid-cols-2 gap-4 ">
+            <div className="col-span-1 p-4 bg-zuma-green rounded-lg">
               <div className="overflow-y-auto max-h-96">
+                <h2 className="text-lg font-semibold mb-4 text-black">
+                  Zuma Products
+                </h2>
                 <input
                   value={searchQuery}
                   onChange={(e) => {
                     setSearchQuery(e.target.value);
                   }}
-                  className="w-full mb-4 p-2 border rounded-md"
+                  className="w-full mb-4 p-2 border rounded-lg text-black"
                   placeholder="Search..."
                 />
                 <table className="min-w-full border-collapse">
                   <thead>
                     <tr className="bg-gray-300">
-                      <th className="px-4 py-2 border">Product Name</th>
-                      <th className="px-4 py-2 border">Product ID</th>
-                      <th className="px-4 py-2 border">Company ID</th>
+                      <th className="px-4 py-2 border text-black">
+                        Product Name
+                      </th>
+                      <th className="px-4 py-2 border text-black">
+                        Product ID
+                      </th>
+                      <th className="px-4 py-2 border text-black">
+                        Company ID
+                      </th>
                     </tr>
                   </thead>
                   <tbody>{tableRows}</tbody>
                 </table>
               </div>
             </div>
-            <div className="col-span-1 bg-gray-200 p-4">
+            <div className="col-span-1 p-4 bg-zuma-green rounded-md">
               {productAnalytics != null ? (
                 <div>
-                  <h2 className="text-lg font-semibold mb-4">
+                  <h2 className="text-lg font-semibold mb-4 text-black">
                     Product Analytics
                   </h2>
                   <table className="min-w-full border-collapse">
                     <thead>
                       <tr className="bg-gray-300">
-                        <th className="px-4 py-2 border">Stock</th>
-                        <th className="px-4 py-2 border">Stored Stock</th>
-                        <th className="px-4 py-2 border">Active Stock</th>
+                        <th className="px-4 py-2 border text-black">Stock</th>
+                        <th className="px-4 py-2 border text-black">
+                          Stored Stock
+                        </th>
+                        <th className="px-4 py-2 border text-black">
+                          Active Stock
+                        </th>
                       </tr>
                     </thead>
-                    <tbody className="bg-gray-400">
+                    <tbody className="bg-white">
                       <tr>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {Math.floor(productAnalytics.stock[0].STOCK)}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {Math.floor(productAnalytics.stock[0].STORED_STOCK)}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {Math.floor(productAnalytics.stock[0].ACTIVE_STOCK)}
                         </td>
                       </tr>
                     </tbody>
                   </table>
-                  <h2 className="text-lg font-semibold mb-4">
+                  <h2 className="text-lg font-semibold mb-4 text-black">
                     Most Recent Shipment
                   </h2>
                   <table className="min-w-full border-collapse">
                     <thead>
                       <tr className="bg-gray-300">
-                        <th className="px-4 py-2 border">Product Name</th>
-                        <th className="px-4 py-2 border">Quantity</th>
-                        <th className="px-4 py-2 border">Date</th>
-                        <th className="px-4 py-2 border">Employee</th>
+                        <th className="px-4 py-2 border text-black">
+                          Product Name
+                        </th>
+                        <th className="px-4 py-2 border text-black">
+                          Quantity
+                        </th>
+                        <th className="px-4 py-2 border text-black">Date</th>
+                        <th className="px-4 py-2 borde text-black">Employee</th>
                       </tr>
                     </thead>
-                    <tbody className="bg-gray-400">
+                    <tbody className="bg-white">
                       <tr>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.shipment.length > 0
                             ? productAnalytics.shipment[0].PRODUCT_NAME
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.shipment.length > 0
                             ? Math.floor(productAnalytics.shipment[0].QUANTITY)
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.shipment.length > 0
                             ? new Date(
                                 productAnalytics.shipment[0].DATE
                               ).toDateString()
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.shipment.length > 0
                             ? productAnalytics.shipment[0].EMPLOYEE_ID
                             : "No Data"}
@@ -210,32 +233,36 @@ export default function ViewInventoryModal(props) {
                       </tr>
                     </tbody>
                   </table>
-                  <h2 className="text-lg font-semibold mb-4">
+                  <h2 className="text-lg font-semibold mb-4 text-black">
                     Most Recent Reduction
                   </h2>
                   <table className="min-w-full border-collapse">
                     <thead>
                       <tr className="bg-gray-300">
-                        <th className="px-4 py-2 border">Quantity</th>
-                        <th className="px-4 py-2 border">Date</th>
-                        <th className="px-4 py-2 border">Employee</th>
+                        <th className="px-4 py-2 border text-black">
+                          Quantity
+                        </th>
+                        <th className="px-4 py-2 border text-black">Date</th>
+                        <th className="px-4 py-2 border text-black">
+                          Employee
+                        </th>
                       </tr>
                     </thead>
-                    <tbody className="bg-gray-400">
+                    <tbody className="bg-white">
                       <tr>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.reduction.length > 0
                             ? productAnalytics.reduction[0].QUANTITY.toFixed(2)
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.reduction.length > 0
                             ? new Date(
                                 productAnalytics.reduction[0].DATE
                               ).toDateString()
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.shipment.length > 0
                             ? productAnalytics.reduction[0].EMPLOYEE_ID
                             : "No Data"}
@@ -243,32 +270,36 @@ export default function ViewInventoryModal(props) {
                       </tr>
                     </tbody>
                   </table>
-                  <h2 className="text-lg font-semibold mb-4">
+                  <h2 className="text-lg font-semibold mb-4 text-black">
                     Most Recent Activation
                   </h2>
                   <table className="min-w-full border-collapse">
                     <thead>
                       <tr className="bg-gray-300">
-                        <th className="px-4 py-2 border">Quantity</th>
-                        <th className="px-4 py-2 border">Date</th>
-                        <th className="px-4 py-2 border">Employee</th>
+                        <th className="px-4 py-2 border text-black">
+                          Quantity
+                        </th>
+                        <th className="px-4 py-2 border text-black">Date</th>
+                        <th className="px-4 py-2 border text-black">
+                          Employee
+                        </th>
                       </tr>
                     </thead>
-                    <tbody className="bg-gray-400">
+                    <tbody className="bg-white">
                       <tr>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.activation.length > 0
                             ? productAnalytics.activation[0].QUANTITY.toFixed(2)
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.activation.length > 0
                             ? new Date(
                                 productAnalytics.activation[0].DATE
                               ).toDateString()
                             : "No Data"}
                         </td>
-                        <td className="px-4 py-2 border">
+                        <td className="px-4 py-2 border text-black">
                           {productAnalytics.activation.length > 0
                             ? productAnalytics.activation[0].EMPLOYEE_ID
                             : "No Data"}
@@ -278,13 +309,13 @@ export default function ViewInventoryModal(props) {
                   </table>
                 </div>
               ) : (
-                <div> No Product Selected </div>
+                <div className="text-black"> No Product Selected </div>
               )}
             </div>
           </div>
-          <div className="flex justify-center items-center mt-4">
+          <div className="flex justify-center items-center mt-4 ">
             <div className="w-full md:w-1/2 bg-gray-200 p-4 text-center px-2 py-2">
-              <h2 className="text-lg font-semibold mb-4">
+              <h2 className="text-lg font-semibold mb-4 text-black">
                 Product Visual - Shipment/Stock
               </h2>
               {productAnalytics != null ? (
