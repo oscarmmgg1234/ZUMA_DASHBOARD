1a85ce7541f7c1eabb8d12c2da7a29d9ce7e9fad
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-01-13 17:29:26 -0800

    added emp

--- src/components/dashboard/pages/employee/main.js
+++ src/components/dashboard/pages/employee/main.js
@@ -84,6 +84,7 @@ const local_data = {
     { name: "Maria Maldonado", e_id: "003222" },
     { name: "Jose Adalberto Enciso", e_id: "230114" },
     { name: "PRINT_ALL", e_id: "PRINT_ALL" },
+    { name: "Jesus Garcia", e_id: "451113" },
   ],
   emp_data: [
     { name: "Oscar Maldonado", e_id: "000002" },
@@ -91,6 +92,7 @@ const local_data = {
     { name: "Juan Pablo", e_id: "001111" },
     { name: "Maria Maldonado", e_id: "003222" },
     { name: "Jose Adalberto Enciso", e_id: "230114" },
+    { name: "Jesus Garcia", e_id: "451113" },
   ],
   emps: [
     "Oscar Maldonado",
