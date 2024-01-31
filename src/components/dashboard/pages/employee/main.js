3c9cfb0a4dd10c3f6961c7c141a7f381abfff0d6
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-01-31 15:06:10 -0800

    added new feature

--- src/components/dashboard/pages/employee/main.js
+++ src/components/dashboard/pages/employee/main.js
@@ -1536,7 +1536,7 @@ const Employee = () => {
                   </label>
                   <select
                     id="employee-select"
-                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md text-black"
                     value={selectedEmployee}
                     onChange={(e) => setSelectedEmployee(e.target.value)}
                   >
